7a5eab2e45fc77cff0661cb097825a8b9cee3028
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-07-01 20:19:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,4 +4,25 @@ function setup()
 {
     canvas = createCanvas(300, 300);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotposes);
 }
+function modelLoaded()
+{
+    console.log("Posenet is loaded");
+}
+
+function take_snapshot()
+{
+    save("Mustachefilterimage.jpg");
+}
+function draw()
+{
+   image(video, 0, 0, 300, 300);
+}
+function gotposes(){}
